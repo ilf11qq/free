d340200e2c6309104e33bb20f9116e57873f7829
ilf <ilf.mav@gmail.com>
2020-10-31 15:29:22 +0300

    minimifaction

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css')
+const cleanCSS = require('gulp-clean-css');
+const imageMin = require('gulp-imagemin');
 //const browserSync = require('browser-sync');
 //const concat = require('gulp-concat');
 
@@ -11,5 +12,20 @@ function style () {
     pipe(gulp.dest('css'));
 }
 
+function minBackgroundImg () {
+    return gulp.src('media/img/background-images/*')
+    .pipe(imageMin())
+    .pipe(gulp.dest('media/background-images'))
+};
+function minContentImg () {
+    return gulp.src('media/img/content-images/*')
+    .pipe(imageMin())
+    .pipe(gulp.dest('media/content-images'))
+};
+function minIMG () {
+    minBackgroundImg();
+    minContentImg();
+}
 
+exports.minIMG = minIMG;
 exports.style = style;
